8e155359cee9842956797754ad107e47c5a66121
potykion <potykion@gmail.com>
2021-08-19 18:13:03 +0300

    grammar-exercises pdf > txt; split by topics

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+
+
 def main():
-    ...
+    grammar_exercises_pdf_path = "data/grammar-exercises.pdf"
+    grammar_keys_pdf_path = "data/grammar-keys.pdf"
+
+    # import PyPDF2
+    # grammar_exercises_pdf = PyPDF2.PdfFileReader()
+    # grammar_keys_pdf = PyPDF2.PdfFileReader("data/grammar-keys.pdf")
+    # grammar_exercises_pdf.getPage(8 - 1).extractText() - хуйню выдает
+
+    # import PyPDF4
+    # grammar_exercises_pdf = PyPDF4.PdfFileReader("data/grammar-exercises.pdf")
+    # grammar_keys_pdf = PyPDF4.PdfFileReader("data/grammar-keys.pdf")
+    # grammar_exercises_pdf.getPage(8 - 1).extractText() - хуйню выдает
+
+    # from pdfrw import PdfReader
+    # grammar_exercises_pdf = PdfReader(fname="data/grammar-exercises.pdf")
+    # grammar_exercises_pdf.getPage(8 - 1).extractText() - хуйню выдает
+
+
+
+    s = "As"
 
 
 if __name__ == '__main__':
